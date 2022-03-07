0d19590ea7edce31328887b3efb4ad1f6d95b3ec
Neill Finn <56132852+Astronot21@users.noreply.github.com>
2022-03-07 22:43:57 +0200

    Added all of the basic files
    Added all of the current files that are in the project. More to still be added. Any changes to these files should update here on this repository main branch and you can just pull them and work on them further.

---
+++ InvaderGameState.java
@@ -0,0 +1,3 @@
+// Represents the state of the game; manages updates every step in the game loop 
+public class InvaderGameState {
+}
---
+++ Invaders.java
@@ -0,0 +1,6 @@
+// Class which contains details outside of game loop
+public class Invaders {
+    public static void main(String[] args) {
+        System.out.println("This just proves that this is working correctly");
+    }
+}
